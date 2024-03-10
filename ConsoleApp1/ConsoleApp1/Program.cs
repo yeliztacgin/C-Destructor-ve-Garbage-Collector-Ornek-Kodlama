2fdf51a7647e57175dd7254e64de5b3b9699f93b
yeliztacgin <145569090+yeliztacgin@users.noreply.github.com>
2024-03-10 20:29:03 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                new Ornek(i);
+                Console.WriteLine("****************");
+                GC.Collect();
+                Console.Read();
+            }
+        }
+
+        class Ornek
+        {
+            int numara;
+            public Ornek(int id)
+            {
+                numara = id;
+                Console.WriteLine(numara + "nesne üretildi");
+
+            }
+
+
+            #region Destructor
+
+            ~Ornek()
+            {
+                Console.WriteLine(numara + "nesne siliniyor");
+
+            }
+            #endregion
+        }
+    }
+}
